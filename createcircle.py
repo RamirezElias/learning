034f943b20269a2230636f5a069adc7013c0517c
carmen <eliasramirez1999@gmail.com>
2014-12-08 19:59:48 -0800

    incremental

--- createcircle.py
+++ createcircle.py
@@ -3,9 +3,10 @@ import turtle
 turtle.home()
 t = turtle
 
-for i in range(4):
-    t.forward(200)
-    t.right(90)
+def square():
+    for i in range(4):
+        t.forward(200)
+        t.right(90)
 
 '''
 t.pu()    
@@ -27,6 +28,7 @@ def dot1():
     t.backward(50)
 
 def dot2():
+    t.pu()
     t.forward(100)
     t.right(90)
     t.forward(50)
@@ -34,9 +36,11 @@ def dot2():
     t.backward(50)
     t.left(90)
     t.backward(100)
+    t.pd()
    
 
 def dot3():
+    t.pu()
     t.forward(150)
     t.right(90)
     t.forward(50)
@@ -44,8 +48,10 @@ def dot3():
     t.backward(50)
     t.left(90)
     t.backward(150)
+    t.pd()
 
 def dot4():
+    t.pu()
     t.forward(50)
     t.right(90)
     t.forward(100)
@@ -53,7 +59,9 @@ def dot4():
     t.backward(100)
     t.left(90)
     t.backward(50)
-def dot5(): 
+    t.pd()
+def dot5():
+    t.pu()
     t.forward(100)
     t.right(90)
     t.forward(100)
@@ -61,75 +69,85 @@ def dot5():
     t.backward(100)
     t.left(90)
     t.backward(100)
-def dot6(): 
+    t.pd()
+def dot6():
+    t.pu()
     t.forward(150)
     t.right(90)
-    t.foward(150)
+    t.forward(150)
     t.dot(30)
     t.backward(100)
     t.left(90)
     t.backward(150)
+    t.pd()
 
-def dot7(): 
+def dot7():
+    t.pu()
     t.forward(50)
     t.right(90)
-    t.foward(150)
+    t.forward(150)
     t.dot(30)
     t.backward(150)
     t.left(90)
     t.backward(50)
+    t.pd()
 
-def dot8(): 
+def dot8():
+    t.pu()
     t.forward(100)
     t.right(90)
-    t.foward(150)
+    t.forward(150)
     t.dot(30)
     t.backward(150)
     t.left(90)
     t.backward(100)
+    t.pd()
 
 def dot9():
+    t.pu()
     t.forward(150)
     t.right(90)
-    t.foward(150)
+    t.forward(150)
     t.dot(30)
     t.backward(150)
     t.left(90)
     t.backward(150)
+    t.pd()
 
 def side1():
     dot5()
     
 
 def side2():
-    dot()
-    dot()
+    dot3()
+    dot7()
     
 def side3():
-    dot()
-    dot()
-    dot()
+    dot3()
+    dot5()
+    dot7()
 def side4():
-    dot()
-    dot()
-    dot()
-    dot()
+    dot1()
+    dot3()
+    dot7()
+    dot9()
 def side5():
-    dot()
-    dot()
-    dot()
-    dot()
-    dot()
-    dot()
+    dot1()
+    dot3()
+    dot5()
+    dot7()
+    dot9()
 def side6():
-    dot()
-    dot()
-    dot()
-    dot()
-    dot()
-    dot()
+    dot1()
+    dot2()
+    dot3()
+    dot7()
+    dot8()
+    dot9()
 
-side1()    
-    
+
+square()
+side2()
+
      
 
--- stripped_down.py
+++ stripped_down.py
@@ -1,14 +1,14 @@
-import pygame, sys
+import pygame
 from pygame.locals import *
 
 # set up pygame
 pygame.init()
-windowsurface = pygame.display.set_mode((500, 400),0,32)
+windowsurface = pygame.display.set_mode((300,400),0,32)
 pygame.display.set_caption('SPACE GAME !')
 
 blue =(0,0,255)
 white = (255,255,255)
-
+red = (255,0,0)
 basicfont = pygame.font.SysFont(None,48)
 
 
@@ -18,14 +18,13 @@ windowsurface.fill(white)
 
 Alien3a = '/home/carmen/elias/Alien3a.png'
 
-pygame.image.load(Alien3a)
+#pygame.image.load(Alien3a)
 
+pygame.draw.rect(windowsurface, blue, ((5,5),(50,50)))
+pygame.draw.circle(windowsurface, blue, (275,50),20,0)
 
+pygame.draw.circle(windowsurface, red, (20,100),20,0)
 pygame.display.update()
-
-pygame.draw.circle(windowsurface, blue, (300,50),20,0)
-
-
 
 running = True
 
@@ -33,6 +32,7 @@ while running:
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
+            
             
 
 
